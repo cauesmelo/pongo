3adbe9df6b0d7c2aefc6df92bbdbc99cf6d3e456
cauesmelo <cauesmelo@gmail.com>
2024-09-10 22:18:07 -0300

    ball movement and collision

--- entity/ball.go
+++ entity/ball.go
@@ -2,17 +2,22 @@ package entity
 
 import (
 	"log"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type ball struct {
-	X      int
-	Y      int
-	size   int
-	sprite *ebiten.Image
+	X         int
+	Y         int
+	size      int
+	sprite    *ebiten.Image
+	velocityX int
+	velocityY int
 }
+
+const ballSpeed = 3
 
 func CreateBall() *ball {
 	img, _, err := ebitenutil.NewImageFromFile("./asset/pongo.png")
@@ -24,17 +29,70 @@ func CreateBall() *ball {
 	size := img.Bounds().Size()
 
 	b := &ball{
-		X:      w/2 - 20,
-		Y:      h/2 - 20,
-		sprite: img,
-		size:   size.X,
+		X:         w/2 - 20,
+		Y:         h/2 - 20,
+		sprite:    img,
+		size:      size.X,
+		velocityX: 0,
+		velocityY: 0,
 	}
 
 	return b
 }
 
-func (b *ball) Update(keys []ebiten.Key, state GameState) {
-	// pass
+func (b *ball) isStoped() bool {
+	return b.velocityX == 0 && b.velocityY == 0
+}
+
+func getRng() int {
+	if rand.Intn(2) == 0 {
+		return ballSpeed
+	} else {
+		return ballSpeed * -1
+	}
+}
+
+func (b *ball) launch() {
+	b.velocityY = getRng()
+	b.velocityX = getRng()
+}
+
+func (b *ball) checkBoundaries(newGame func()) {
+	w, h := ebiten.WindowSize()
+
+	if b.Y <= 0 || b.Y >= h-b.size {
+		b.velocityY *= -1
+	}
+
+	if b.X <= 0 || b.X >= w-b.size {
+		newGame()
+	}
+}
+
+func (b *ball) checkPaddleCollision(entities []Entity) {
+	for _, e := range entities {
+		paddle, isPaddle := e.(*Paddle)
+
+		if isPaddle {
+			if b.X <= paddle.X+paddle.width && b.Y >= paddle.Y && b.Y <= paddle.Y+paddle.height {
+				b.velocityX *= -1
+			}
+		}
+	}
+}
+
+func (b *ball) Update(game *Game) {
+	if game.state.Started && b.isStoped() {
+		b.launch()
+		return
+	}
+
+	b.checkBoundaries(game.NewGame)
+	b.checkPaddleCollision(game.entities)
+
+	b.X += b.velocityX
+	b.Y += b.velocityY
+
 }
 
 func (b *ball) Draw(screen *ebiten.Image) {
