92d47333f84325cc622f77dbdd3e707009e9650f
cauesmelo <cauesmelo@gmail.com>
2024-09-10 22:17:48 -0300

    move game logic to entity

---
+++ entity/game.go
@@ -0,0 +1,60 @@
+package entity
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Game struct {
+	entities []Entity
+	keys     []ebiten.Key
+	state    GameState
+}
+
+func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+
+	if !g.state.Started && ebiten.IsKeyPressed(ebiten.KeySpace) {
+		g.state.Started = true
+	}
+
+	for _, e := range g.entities {
+		e.Update(g)
+	}
+
+	return nil
+}
+
+func (g *Game) GameOver() {
+	g.state.Started = false
+	g.entities = []Entity{
+		CreateBall(),
+		CreatePaddle(),
+		CreateText("Press SPACE to start"),
+	}
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, e := range g.entities {
+		e.Draw(screen)
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480
+}
+
+func (g *Game) NewGame() {
+	initialState := GameState{
+		Started: false,
+	}
+
+	entities := []Entity{
+		CreateBall(),
+		CreatePaddle(),
+		CreateText("Press SPACE to start"),
+	}
+
+	g.entities = entities
+	g.state = initialState
+}
--- main.go
+++ main.go
@@ -5,58 +5,16 @@ import (
 
 	"github.com/cauesmelo/pongo/entity"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct {
-	entities []entity.Entity
-	keys     []ebiten.Key
-	state    entity.GameState
-}
-
-func (g *Game) Update() error {
-	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
-
-	if !g.state.Started && ebiten.IsKeyPressed(ebiten.KeySpace) {
-		g.state.Started = true
-	}
-
-	for _, e := range g.entities {
-		e.Update(g.keys, g.state)
-	}
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	for _, e := range g.entities {
-		e.Draw(screen)
-	}
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
-}
-
-func startup() *Game {
+func startup() *entity.Game {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Pongo")
 
-	entities := []entity.Entity{
-		entity.CreateBall(),
-		entity.CreatePaddle(),
-		entity.CreateText("Press SPACE to start"),
-	}
+	game := &entity.Game{}
+	game.NewGame()
 
-	initialState := entity.GameState{
-		Started: false,
-	}
-
-	return &Game{
-		entities: entities,
-		keys:     []ebiten.Key{},
-		state:    initialState,
-	}
+	return game
 }
 
 func main() {
